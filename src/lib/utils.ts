0eca2ce5501d3d25377eae9ec9794659c4d0c087
Siddharth <siddharthmohanty23@gmail.com>
2024-09-14 16:32:00 +0530

    utilsChange

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -8,7 +8,7 @@ export function cn(...inputs: ClassValue[]) {
 export const fetchUrl =
   process.env.NODE_ENV === "development"
     ? "http://localhost:3000/api"
-    : "https://next-blog-cj.vercel.app/api";
+    : "https://heisenberg-next-blog.vercel.app/api";
 
 type ResponseData = {
   category: string;
